137dc9b14a557136d756501faf7829df832bbffc
DeathStorm <Death_Storm@web.de>
2015-09-12 18:56:48 +0200

    My First TestScene

---
+++ Assets/Resources/Scripts/MouseLook.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public enum RotationAxes { MouseXAndY, MouseX, MouseY }
+
+    public RotationAxes rotateAroundAxes = RotationAxes.MouseXAndY;
+
+    public float sensivityX = 15.0f;
+    public float sensivityY = 15.0f;
+
+    public float minX = -360.0f;
+    public float maxX = 360.0f;
+
+    public float minY = -60.0f;
+    public float maxY = 60.0f;
+
+    Transform myTransform;
+    Rigidbody myRigidBody;
+
+    float rotationY = 0.0f;
+
+    void Awake()
+    {
+        myTransform = this.transform;
+        myRigidBody = this.GetComponent<Rigidbody>();
+
+        if (myRigidBody) myRigidBody.freezeRotation = true;
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotateAroundAxes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = myTransform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensivityX;
+            rotationX = Mathf.Clamp(rotationX, minX, maxX);
+
+            rotationY += Input.GetAxis("Mouse Y") * sensivityY;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+
+            myTransform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (rotateAroundAxes == RotationAxes.MouseX)
+        {
+            float rotationX = myTransform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensivityX;
+            rotationX = Mathf.Clamp(rotationX, minX, maxX);
+
+            myTransform.localEulerAngles = new Vector3(0, rotationX, 0);
+        }
+        else if (rotateAroundAxes == RotationAxes.MouseY)
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensivityY;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+
+            myTransform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+        }
+
+    }
+}
---
+++ Assets/Resources/Scripts/Player_Move.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Move : MonoBehaviour
+{
+
+    float walkSpeed = 5.0f;
+    float runMultiplier = 2.0f;
+    float jumpSpeed = 1000.0f;
+
+    bool isGrounded = true;
+
+    Rigidbody myRigidBody;
+    Camera myCamera;
+
+    void Awake()
+    {
+        myRigidBody = this.GetComponent<Rigidbody>();
+        myCamera = this.GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+        Rotate();
+
+    }
+
+    void Jump()
+    {
+        RaycastHit rayHit;
+        if (Physics.Raycast(transform.position, -transform.up, out rayHit, this.transform.localScale.y / 2))
+        { isGrounded = true; }
+        else
+        { isGrounded = false; }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(isGrounded);
+            if (isGrounded)
+            {
+                isGrounded = false;
+                myRigidBody.AddForce(Vector3.up * jumpSpeed);
+            }
+        }
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3();
+        if (Input.GetAxisRaw("Vertical") > 0) { movement += myRigidBody.transform.forward; }
+        if (Input.GetAxisRaw("Vertical") < 0) { movement -= myRigidBody.transform.forward; }
+        if (Input.GetAxisRaw("Horizontal") > 0) { movement += myRigidBody.transform.right; }
+        if (Input.GetAxisRaw("Horizontal") < 0) { movement -= myRigidBody.transform.right; }
+
+
+        movement = movement.normalized * walkSpeed * Time.deltaTime;
+        //if (Input.GetKeyDown(KeyCode.LeftShift)) { movement *= runMultiplier; }
+        if (Input.GetAxisRaw("Vertical") > 0 && Input.GetKey(KeyCode.LeftShift)) { movement *= runMultiplier; }
+
+
+        myRigidBody.MovePosition(myRigidBody.transform.position + movement);
+    }
+
+
+    void Rotate()
+    {
+        Vector3 rotation = new Vector3();
+        rotation.x = Input.GetAxisRaw("Mouse X");
+        rotation.y = Input.GetAxisRaw("Mouse Y");
+
+
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
